9e44a89ffea6bdb8dde5522a067d81973e888370
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2025-01-23 15:53:53 -0600

    feat: date format

--- src/Components/EntryTable.jsx
+++ src/Components/EntryTable.jsx
@@ -3,6 +3,16 @@ const EntryTable = ({ entries, onSelected }) => {
     onSelected(e);
   };
 
+  function formatDate(dateString) {
+    const date = new Date(dateString); // Crea un objeto Date a partir de la cadena
+    const day = String(date.getDate()).padStart(2, '0'); // Agrega ceros al día si es necesario
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Los meses son base 0
+    const year = date.getFullYear(); // Obtén el año
+  
+    return `${day}/${month}/${year}`; // Devuelve la fecha en formato DD/MM/YYYY
+  }
+  
+
   return (
     <table className="table-auto w-full border-collapse">
       <thead className="bg-gray-100">
@@ -22,7 +32,7 @@ const EntryTable = ({ entries, onSelected }) => {
               className="border-t hover:bg-slate-200 transition duration-500"
               onClick={() => setSelected(entry)}
             >
-              <td className="px-4 py-2">{entry.created}</td>
+              <td className="px-4 py-2">{formatDate(entry.created)}</td>
               <td className="px-4 py-2">{entry.author}</td>
               <td className="px-4 py-2">{entry.carPlate}</td>
               <td className="px-4 py-2">{entry.notes}</td>
