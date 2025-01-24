8367d320683e9cdcbfab9508485518ea3bce7a07
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2025-01-24 14:43:16 -0600

    feat: Add Car

--- src/Pages/CarList.jsx
+++ src/Pages/CarList.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import CarTable from "../Components/CarTable";
+import CarAdd from "../Components/CarAdd";
 
 const CarList = () => {
+  const [addCar, setAddCar] = useState(false);
+
   return (
     <div>
       <h1 className="text-3xl font-medium">Vehículos</h1>
-      <div>
+      {!addCar && <button className="border border-green-400 text-green-400 bg-white p-2 rounded-md" onClick={() => setAddCar(true)}>Agregar Auto</button>}
+   
+      {addCar && <CarAdd OnClose={setAddCar} /> }
+      <div className="flex flex-col">
         <CarTable />
       </div>
+ 
     </div>
   );
 };
