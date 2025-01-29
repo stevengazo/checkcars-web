dcdc13c0f8dc386ac5763a7998d0bb477a85f8b3
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2025-01-29 10:06:58 -0600

    fix: date format

--- src/Components/EntryTable.jsx
+++ src/Components/EntryTable.jsx
@@ -12,9 +12,12 @@ const EntryTable = ({ entries, onSelected }) => {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+  
+    return `${day}/${month}/${year} - ${hours}:${minutes}`;
   };
-
+  
   const sortedEntries = React.useMemo(() => {
     let sortableItems = [...(entries || [])];
     if (sortConfig.key !== null) {
